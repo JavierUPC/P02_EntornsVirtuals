c642986489165fe93b6eb5e20b8497f130fc5c82
galypau6 <pau.galy@estudiantat.upc.edu>
2024-12-02 16:25:42 +0100

    DirecciónNormalizada+VelocidadFloat

--- Assets/Scripts/Initializer.cs
+++ Assets/Scripts/Initializer.cs
@@ -1,14 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Initialation : MonoBehaviour
+public class ObjectMover : MonoBehaviour
 {
     [SerializeField] private GameObject objectToMove;
-    [SerializeField] private Vector3 velocity;
-    [SerializeField] private float delay = 0.5f; //Temps en segons (li he posat delay pq és el q tarda en inicialitzar)
+    [SerializeField] private Transform target;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float delay = 0.5f; //x si volem delay
 
     private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,30 +16,24 @@ public class Initialation : MonoBehaviour
         {
             rb = objectToMove.GetComponent<Rigidbody>();
 
-            //per afegir rigid body si no en té
+            //x afegir rb si no té
             if (rb == null)
             {
                 rb = objectToMove.AddComponent<Rigidbody>();
             }
-
         }
 
         Invoke(nameof(InitializeObject), delay);
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        
     }
 
     private void InitializeObject()
     {
-        if (rb != null)
+        if (rb != null && target != null)
         {
-            rb.velocity = velocity; //per donar-li velocitat
+            //calcul vector dir normalizado
+            Vector3 direction = (target.position - objectToMove.transform.position).normalized;
+
+            rb.velocity = direction * speed;
         }
     }
 }
