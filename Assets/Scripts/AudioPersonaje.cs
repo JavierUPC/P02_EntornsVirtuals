ad02f15a70fc90e01af5d5b0553f8c03eb41d94d
gmlluk <llucgrifol@gmail.com>
2024-12-08 12:35:17 +0100

    sonido salto

--- Assets/Scripts/AudioPersonaje.cs
+++ Assets/Scripts/AudioPersonaje.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class AudioPersonaje : MonoBehaviour
 {
-    public AudioClip step1, step2;
+    public AudioClip step1, step2, salto;
     public AudioSource audioSource1; // Cambié el nombre para ser consistente
 
     public void PlayPaso1() // Desde el animador
     {
-        audioSource1.clip = step1; 
+        audioSource1.clip = step1;
+        audioSource1.volume = 30;
         audioSource1.Play();
         // Debug.Log("Played Step 1");
     }
 
     public void PlayPaso2() // Desde el animador
     {
-        audioSource1.clip = step2; 
+        audioSource1.clip = step2;
+        audioSource1.volume = 30;
         audioSource1.Play();
         // Debug.Log("Played Step 2");
+    }
+
+    public void PlaySalto()
+    {
+        audioSource1.clip = salto;
+        audioSource1.volume = 40;
+        audioSource1.Play();
+
     }
 }
 
