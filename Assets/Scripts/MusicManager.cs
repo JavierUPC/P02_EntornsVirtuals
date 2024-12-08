eb9b20fcbe925c8e2feb9033f8793ce136938238
Angl0d <javier.ingelius@estudiantat.upc.edu>
2024-12-08 22:33:05 +0100

    MUSICA VAMOOOOOS

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -7,13 +7,21 @@ public class MusicManager : MonoBehaviour
     public AudioClip inversa, normal;
     private AudioSource musicPlayer;
     public GameObject player;
+
+    private void Start()
+    {
+        musicPlayer = GetComponent<AudioSource>();
+        musicPlayer.clip = normal;
+        musicPlayer.Play();
+    }
+
     private void FixedUpdate()
     {
         if (player.GetComponent<DimensionChange>().Dystopian() && musicPlayer.clip == normal)
         {
             InverseMusic();
         }
-        else if (player.GetComponent<DimensionChange>().Dystopian() && musicPlayer.clip == inversa)
+        else if (!player.GetComponent<DimensionChange>().Dystopian() && musicPlayer.clip == inversa)
         {
             NormalMusic();
         }
